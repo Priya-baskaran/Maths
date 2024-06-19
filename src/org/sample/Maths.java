6b9924dc04f3ce6e8afacccc30f8c5f7ff2b065f
Priya-baskaran <priyavasanthi130899@gmail.com>
2024-06-19 16:49:09 +0530

    Maths

---
+++ src/org/sample/Maths.java
@@ -0,0 +1,19 @@
+package org.sample;
+
+public class Maths {
+private void sum() {
+	// TODO Auto-generated method stub
+	System.out.println("addition");
+
+}
+ private void add() {
+	// TODO Auto-generated method stub
+	 System.out.println("divion");
+
+}
+ public static void main(String[] args) {
+	Maths a=new Maths();
+	a.sum();
+	a.add();
+}
+}
